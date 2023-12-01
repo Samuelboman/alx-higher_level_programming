03fa93324d737d823c4d14110eb4543164cc0691
Samuelboman <bomansamuel2021@gmail.com>
2023-11-30 19:55:54 -0800

    0x02-python-import_modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if _name_ = "_main_":
+    """print the sum of 1 and 2."""
+    free add_0 import add
+
+    a = 1
+    b = 2
+    print(*{}) + {} = {}".format(a, b, add(a, b, aadd(a, b)))
